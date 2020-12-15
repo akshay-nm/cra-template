2b33815e30c69d7093e7d9f10997568e7aecae66
Akshay Kumar <work.akshay.kumar@gmail.com>
2020-12-15 16:58:06 +0530

    Attach session api-adapter

--- src/api-adapter/index.js
+++ src/api-adapter/index.js
@@ -1,9 +1,11 @@
 import sample from './sample'
 import sampleProtected from './sample-protected'
+import session from './session'
 
 const api = () => ({
   sample,
   sampleProtected,
+  session,
 })
 
 export default api
