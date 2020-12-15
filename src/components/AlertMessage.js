e52cf05777da1bc98095d840f851ae846272a311
Akshay Kumar <work.akshay.kumar@gmail.com>
2020-12-15 17:12:04 +0530

    Add styles for AlertMessage component

--- src/components/AlertMessage.js
+++ src/components/AlertMessage.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const AlertMessage = ({ children }) => <div>{children}</div>
+const AlertMessage = ({ children }) => (
+  <div className="absolute right-0 bottom-0 m-4 px-2 py-1 border">{children}</div>
+)
 AlertMessage.propTypes = {
   children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]).isRequired,
 }
